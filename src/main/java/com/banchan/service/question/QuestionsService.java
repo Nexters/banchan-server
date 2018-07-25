32916adf0b1664f7496ae44554fbc13708c48e1a
Jonghyon Seo <sjonghyon@gmail.com>
2018-07-25 16:23:42 +0900

    image 관련은 잠시 뒤로 미룸

--- src/main/java/com/banchan/service/question/QuestionsService.java
+++ src/main/java/com/banchan/service/question/QuestionsService.java
@@ -19,8 +19,6 @@ public class QuestionsService {
                     DetailType.valueOf(detail.getType()),
                     detail.getContent()
             );
-
         return map;
     }
-
 }
