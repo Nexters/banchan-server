33ccd6598aa636e891047e6da9141b3ba593bc86
INHYCUK <dlsgur1447@gmail.com>
2018-07-27 23:07:10 +0900

    [추가]모든 Entity 생성/수정시간 상속받는 BaseTimeEntity

--- src/main/java/com/banchan/Application.java
+++ src/main/java/com/banchan/Application.java
@@ -2,9 +2,11 @@ package com.banchan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.IOException;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 
