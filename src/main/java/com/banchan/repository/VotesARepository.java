42e97b8cc03e021b5c02b265fb3bcffa4c84dbe2
shouwn <sjonghyon@gmail.com>
2018-08-11 15:42:07 +0900

    @Param 추가

--- src/main/java/com/banchan/repository/VotesARepository.java
+++ src/main/java/com/banchan/repository/VotesARepository.java
@@ -4,6 +4,7 @@ import com.banchan.model.dto.VoteCountData;
 import com.banchan.model.entity.VotesA;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -11,5 +12,5 @@ import java.util.concurrent.CompletableFuture;
 public interface VotesARepository extends JpaRepository<VotesA, Integer> {
 
     @Query("SELECT NEW com.banchan.model.dto.VoteCountData(v.questionId, COUNT(v)) FROM VotesA v WHERE v.questionId IN (:questionIds) GROUP BY v.questionId")
-    CompletableFuture<List<VoteCountData>> countByQuestionIdInGroupByQuestion(List<Integer> questionIds);
+    CompletableFuture<List<VoteCountData>> countByQuestionIdInGroupByQuestion(@Param("questionIds") List<Integer> questionIds);
 }
