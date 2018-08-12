2223dc91ca2630143ee0075d6bf12f88553cf94e
high <high.lee@kakaocorp.com>
2018-08-12 13:53:54 +0900

    누락 추가

--- src/main/java/com/banchan/controller/UserController.kt
+++ src/main/java/com/banchan/controller/UserController.kt
@@ -4,17 +4,15 @@ import com.banchan.model.entity.User
 import com.banchan.model.response.CommonResponse
 import com.banchan.service.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@RequestMapping("/user")
 @RestController
 open class UserController {
     @Autowired lateinit var userService : UserService
 
     @ResponseBody
-    @PostMapping("")
+    @PostMapping
     fun save (@RequestBody user: User): CommonResponse<User>? {
         return CommonResponse.success(user)
     }
