a468d91fbbe6672cc11e44438f10b8c98084790e
shouwn <sjonghyon@gmail.com>
2018-08-24 23:28:28 +0900

    vote count 오류 수정

--- src/main/java/com/banchan/repository/QuestionCardDataRepository.java
+++ src/main/java/com/banchan/repository/QuestionCardDataRepository.java
@@ -11,25 +11,27 @@ public interface QuestionCardDataRepository extends JpaRepository<QuestionCardDa
 
     @Query(
             value = "SELECT qu.*, qd.id detail_id, qd.type detail_type, qd.content detail_content FROM (  " +
-                    "  SELECT q.*, u.prefix, u.postfix,  " +
-                    "    COUNT(DISTINCT r.id) reviews,  " +
-                    "    SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a,  " +
-                    "    SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b  " +
+                    "  SELECT q.*, u.prefix, u.postfix, " +
+                    "    COUNT(DISTINCT r.id) reviews " +
                     "  FROM (  " +
-                    "    SELECT q.*  " +
-                    "    FROM questions q  " +
-                    "    LEFT JOIN (SELECT question_id FROM votes WHERE user_id = :userId) v  " +
+                    "    SELECT q.*, " +
+                    "      SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
+                    "      SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    FROM (" +
+                    "      SELECT q.*  " +
+                    "      FROM questions q  " +
+                    "      LEFT JOIN (SELECT DISTINCT question_id FROM votes WHERE user_id = :userId) v  " +
+                    "        ON q.id = v.question_id  " +
+                    "      WHERE v.question_id IS NULL AND  " +
+                    "        q.random_order > :randomOrder AND  " +
+                    "        q.decision IS NULL AND  " +
+                    "        q.report_state = 0 AND  " +
+                    "        q.user_id != :userId " +
+                    "      ORDER BY q.random_order " +
+                    "      LIMIT :counting) q " +
+                    "    LEFT JOIN votes v  " +
                     "      ON q.id = v.question_id  " +
-                    "    WHERE v.question_id IS NULL AND  " +
-                    "      q.random_order > :randomOrder AND  " +
-                    "      q.decision IS NULL AND  " +
-                    "      q.report_state = 0 AND  " +
-                    "      q.user_id != :userId " +
-                    "    ORDER BY q.random_order " +
-                    "    LIMIT :counting " +
-                    "  ) q  " +
-                    "  LEFT JOIN votes v  " +
-                    "    ON q.id = v.question_id  " +
+                    "    GROUP BY q.id) q  " +
                     "  LEFT JOIN reviews r  " +
                     "    ON q.id = r.question_id  " +
                     "  LEFT JOIN usernames u  " +
@@ -47,19 +49,23 @@ public interface QuestionCardDataRepository extends JpaRepository<QuestionCardDa
             value = "SELECT qu.*, qd.id detail_id, qd.type detail_type, qd.content detail_content " +
                     "FROM ( " +
                     "  SELECT q.*, u.prefix, u.postfix, " +
-                    "    COUNT(DISTINCT r.id) reviews, " +
-                    "    SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
-                    "    SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    COUNT(DISTINCT r.id) reviews " +
                     "  FROM ( " +
-                    "    SELECT q.*, v.vote_time " +
-                    "    FROM questions q " +
-                    "    JOIN (SELECT question_id, vote_time FROM votes WHERE user_id = :userId) v " +
+                    "    SELECT q.*, " +
+                    "      SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
+                    "      SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    FROM (" +
+                    "      SELECT q.*, v.vote_time " +
+                    "      FROM questions q " +
+                    "      JOIN (SELECT question_id, vote_time FROM votes WHERE user_id = :userId GROUP BY question_id) v " +
+                    "        ON q.id = v.question_id " +
+                    "      WHERE report_state = 0 " +
+                    "      ORDER BY v.vote_time DESC " +
+                    "      LIMIT :start, :counting) q " +
+                    "    LEFT JOIN votes v " +
                     "      ON q.id = v.question_id " +
-                    "    WHERE report_state = 0 " +
-                    "    ORDER BY v.vote_time DESC " +
-                    "    LIMIT :start, :counting) q " +
-                    "  LEFT JOIN votes v " +
-                    "    ON q.id = v.question_id " +
+                    "    GROUP BY q.id" +
+                    "  ) q" +
                     "  LEFT JOIN reviews r " +
                     "    ON q.id = r.question_id " +
                     "  LEFT JOIN usernames u " +
@@ -77,17 +83,19 @@ public interface QuestionCardDataRepository extends JpaRepository<QuestionCardDa
             value = "SELECT qu.*, qd.id detail_id, qd.type detail_type, qd.content detail_content " +
                     "FROM ( " +
                     "  SELECT q.*, u.prefix, u.postfix, " +
-                    "    COUNT(DISTINCT r.id) reviews, " +
-                    "    SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
-                    "    SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    COUNT(DISTINCT r.id) reviews " +
                     "  FROM ( " +
-                    "    SELECT * " +
-                    "    FROM questions " +
-                    "    WHERE user_id = :userId AND report_state = 0 " +
-                    "    ORDER BY decision IS NULL DESC, id DESC " +
-                    "    LIMIT :start, :counting) q " +
-                    "  LEFT JOIN votes v " +
-                    "    ON q.id = v.question_id " +
+                    "    SELECT q.*, " +
+                    "      SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
+                    "      SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    FROM questions q" +
+                    "    LEFT JOIN votes v " +
+                    "      ON q.id = v.question_id " +
+                    "    WHERE q.user_id = :userId AND q.report_state = 0 " +
+                    "    GROUP BY q.id " +
+                    "    ORDER BY q.decision IS NULL DESC, q.id DESC " +
+                    "    LIMIT :start, :counting " +
+                    "  ) q " +
                     "  LEFT JOIN reviews r " +
                     "    ON q.id = r.question_id " +
                     "  LEFT JOIN usernames u " +
@@ -105,15 +113,17 @@ public interface QuestionCardDataRepository extends JpaRepository<QuestionCardDa
             value = "SELECT qu.*, qd.id detail_id, qd.type detail_type, qd.content detail_content " +
                     "FROM ( " +
                     "  SELECT q.*, u.prefix, u.postfix, " +
-                    "    COUNT(DISTINCT r.id) reviews, " +
-                    "    SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
-                    "    SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    COUNT(DISTINCT r.id) reviews " +
                     "  FROM ( " +
-                    "    SELECT * " +
-                    "    FROM questions " +
-                    "    WHERE id = :questionId) q " +
-                    "  LEFT JOIN votes v " +
-                    "    ON q.id = v.question_id " +
+                    "    SELECT q.*, " +
+                    "      SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) a, " +
+                    "      SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) b " +
+                    "    FROM questions q " +
+                    "    LEFT JOIN votes v " +
+                    "      ON q.id = v.question_id " +
+                    "    WHERE q.id = :questionId " +
+                    "    GROUP BY q.id " +
+                    "  ) q  " +
                     "  LEFT JOIN reviews r " +
                     "    ON q.id = r.question_id " +
                     "  LEFT JOIN usernames u " +
